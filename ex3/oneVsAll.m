5922acba0ea0c83ed5f507aa7e5536f2a627b024
shendehaizi163@gmail.com <shendehaizi163@gmail.com>
2017-09-08 12:21:19 +0800

    oneVsAll

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -34,6 +34,16 @@ X = [ones(m, 1) X];
 %       fmincg works similarly to fminunc, but is more efficient when we
 %       are dealing with large number of parameters.
 %
+     for c=1:num_labels
+       initial_theta = zeros(n + 1, 1);
+       options = optimset('GradObj', 'on', 'MaxIter', 50);
+       [theta] = ...
+           fmincg (@(t)(lrCostFunction(t, X, (y==c), lambda)), ...
+                   initial_theta, options);
+        %fprintf('initial_theta: \n %f', initial_theta)
+        fprintf('now c is : %f', c)
+        all_theta(c,:) = initial_theta
+      end
 % Example Code for fmincg:
 %
 %     % Set Initial theta
@@ -49,12 +59,6 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
-
-     initial_theta = zeros(n + 1, 1);
-     options = optimset('GradObj', 'on', 'MaxIter', 50);
-     [theta] = ...
-         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
-                 initial_theta, options);
 
 
 
