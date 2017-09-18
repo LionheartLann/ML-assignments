4b57b829c2e1f04e082160be21812176f39daf24
shendehaizi163@gmail.com <shendehaizi163@gmail.com>
2017-09-18 16:07:49 +0800

    the only thing i've done to learningCurve is to set lambda to 0..

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -50,8 +50,8 @@ error_val   = zeros(m, 1);
       % error_train(i) and error_val(i)
       ....
     [theta] = trainLinearReg( X(1:i,:), y(1:i), lambda);
-    error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i), theta, lambda);
-    error_val(i) = linearRegCostFunction(Xval, yval, theta, lambda);
+    error_train(i) = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
       
   end
 
